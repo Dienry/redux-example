0fbe631e531dbe118688353b47c6dd476bda752d
Diego Santiago Vega <diego_svega@hotmail.com>
2019-09-23 22:55:30 -0300

    First steps on understanding Redux

--- redux-example/src/index.js
+++ redux-example/src/index.js
@@ -3,6 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {createStore} from 'redux';
+
+//ACTION -> Describes what you want to do
+const increment = () => {
+    return {
+        type: 'INCREMENT'
+    }
+}
+
+const decrement = () => {
+    return {
+        type: 'DECREMENT'
+    }
+}
+
+//REDUCER -> Describes how your action transform the state to the next state
+const counter = (state = 0, action) => {
+    switch(action.type){
+        case 'INCREMENT':
+            return state + 1;
+        case 'DECREMENT':
+            return state - 1;
+    }
+}
+
+//STORE -> Globalized state
+let store = createStore(counter);
+
+//Display it on the console
+store.subscribe(() => console.log(store.getState()));
+
+//DISPATCH -> Execute the action
+store.dispatch(increment());
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
